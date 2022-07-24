4b4be3dbdf3c763ee2b6efa38d6d37e14bee1d76
Dhrumil Patel <dhrumilp15@gmail.com>
2022-07-24 14:10:44 -0400

    Updates

--- pages/sentiment_analysis.py
+++ pages/sentiment_analysis.py
@@ -6,38 +6,42 @@ import seaborn as sns
 sns.set()
 
 @st.cache
+def make_means(*dfs):
+    means = []
+    for df in dfs:
+        average_sentiment = df.groupby('month_year').mean()
+        average_sentiment['averaged_sentiment'] = (average_sentiment.huggingface_sentiment + average_sentiment.spacy_sentiment) / 2
+        means.append(average_sentiment)
+    return means
+
+@st.cache
 def load_data():
     analyst_ratings = pd.read_csv('data/analyst_ratings_monthly_avg_sentiment.csv')
     partner_headlines = pd.read_csv('data/partner_headlines_monthly_avg_sentiment.csv')
-    plt.rc('axes', labelsize=12)
-    return analyst_ratings, partner_headlines
-
+    analyst_means, headline_means = make_means(analyst_ratings, partner_headlines)
+    return analyst_means, headline_means
 
 def show_data(df):
-    months = df.month_year.sort_values().unique()
+    months = df.index
     fig, ax = plt.subplots(figsize=(50, 30), dpi=300)
-    average_sentiment = df.groupby('month_year').mean()
-    average_sentiment['averaged_sentiment'] = (average_sentiment.huggingface_sentiment + average_sentiment.spacy_sentiment) / 2
-    ax.plot(months, average_sentiment.huggingface_sentiment)
-    ax.plot(months, average_sentiment.spacy_sentiment)
-    ax.plot(months, average_sentiment.averaged_sentiment)
+    ax.plot(months, df.huggingface_sentiment)
+    ax.plot(months, df.spacy_sentiment)
+    ax.plot(months, df.averaged_sentiment)
     plt.legend(['distilRoBERTa Sentiment', 'SpaCy Sentiment', 'Averaged Sentiment'], prop={'size': 40})
     plt.title('Average Monthly Sentiment', fontsize=50)
     plt.xlabel('Time', fontsize=50)
     plt.ylabel('Sentiment', fontsize=50)
     plt.xticks(rotation=90)
     st.pyplot(fig)
-    return average_sentiment
 
-
-analyst_ratings, partner_headlines = load_data()
+analyst_means, headline_means = load_data()
 
 st.title("Sentiment Analysis")
 st.subheader("Average Monthly Sentiment of Analyst Ratings")
-show_data(analyst_ratings)
+show_data(analyst_means)
 
 st.markdown("It's worth pointing out that the distilRoBERTa-predicted sentiment has significantly more variance than the "
             "SpaCy-predicted sentiment")
 
 st.subheader("Average Monthly Sentiment of News Headlines")
-show_data(partner_headlines)
+show_data(headline_means)
